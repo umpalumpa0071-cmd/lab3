db3a22b3dd03e0f616aa08dd09b46994b27b6977
umpalumpa0071-cmd <umpalumpa0071@gmail.com>
2026-03-22 14:24:30 +0500

    Add files via upload

---
+++ labb3/labb3/Program.fs
@@ -0,0 +1,52 @@
+﻿(*
+open System
+
+let last x = x % 10
+
+[<EntryPoint>]
+let main args =
+    printf "сколько чисел вы хотите ввести? "
+    let n = int(Console.ReadLine())
+
+    let numbers =
+        [ for i in 1 .. n do
+            printf "Введите число %d: " i
+            let num = int(Console.ReadLine())
+            yield num ]
+
+    let lastn = numbers |> List.map last
+
+    printfn "Последние цифры введённых чисел: %A" lastn
+    0
+    *)
+    (*
+open System
+
+
+let last x = x % 10
+
+let sum d acc x =
+    if last x = d then acc + x
+    else acc
+
+[<EntryPoint>]
+let main args =
+
+    printf "Сколько чисел вы хотите ввести? "
+    let n = int(Console.ReadLine())
+
+    let numbers =
+        [ for i in 1 .. n do
+            printf "Введите число %d: " i
+            let num = int(Console.ReadLine())
+            yield num ]
+        |> Seq.ofList
+
+    printf "Введите цифру для поиска: "
+    let d = int(Console.ReadLine())
+
+    let res = Seq.fold (sum d) 0 numbers
+
+    printfn "Сумма чисел с последней цифрой %d = %d" d res
+    0
+    *)
