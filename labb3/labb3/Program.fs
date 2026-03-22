2337a8efa74b880308c536dc38660355e1fdf9e6
umpalumpa0071-cmd <umpalumpa0071@gmail.com>
2026-03-22 20:13:56 +0500

    Update Program.fs

--- labb3/labb3/Program.fs
+++ labb3/labb3/Program.fs
@@ -24,32 +24,30 @@ let main args =
     *)
     (*
 open System
-
-
-let last x = x % 10
-
-let sum d acc x =
-    if last x = d then acc + x
-    else acc
+let sum seq m =
+    let result =
+        Seq.fold (fun acc x ->
+            if abs x % 10 = m then acc + x
+            else acc
+        ) 0 seq
+    printf "сумма = %d\n" result
 
 [<EntryPoint>]
 let main args =
+    printf "Введите количество элементов n: "
+    let n = int (Console.ReadLine())
 
-    printf "Сколько чисел вы хотите ввести? "
-    let n = int(Console.ReadLine())
+    let seq =
+        seq {
+            for i in 1..n do
+                printf "Введите элемент %d: " i
+                let x = int (Console.ReadLine())
+                yield x
+        }
 
-    let numbers =
-        [ for i in 1 .. n do
-            printf "Введите число %d: " i
-            let num = int(Console.ReadLine())
-            yield num ]
-        |> Seq.ofList
+    printf "Введите цифру: "
+    let d = int (Console.ReadLine())
 
-    printf "Введите цифру для поиска: "
-    let d = int(Console.ReadLine())
-
-    let res = Seq.fold (sum d) 0 numbers
-
-    printfn "Сумма чисел с последней цифрой %d = %d" d res
+    sum seq d
     0
     *)
